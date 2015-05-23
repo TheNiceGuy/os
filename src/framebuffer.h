e014f2a254d224a2377ea37e803e25273e5532ae
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2015-05-23 18:52:29 -0400

    Some small fixes and add inb function

--- src/framebuffer.h
+++ src/framebuffer.h
@@ -12,8 +12,8 @@ void fb_clear();
  *  @param x The x position of the cursor
  *  @param y The y position of the cursor
  *  @return 0 if everything is successful
- *  @return 1 if the width is out of bound
- *  @return 2 if the height is out of bound
+ *          1 if the width is out of bound
+ *          2 if the height is out of bound
  */
 int fb_move_cursor(short x, short y);
 
--- src/io.s
+++ src/io.s
@@ -1,4 +1,5 @@
 global outb
+global inb
 
 section .text
 align 4
@@ -8,7 +9,16 @@ align 4
 ;        [esp+4] [ word] the I/O port
 ;        [esp  ] [dword] the return address
 outb:
-    mov al, [esp+8]
-    mov dx, [esp+4]
-    out dx, al
-    ret
+    mov al, [esp+8]   ; move the data byte into al
+    mov dx, [esp+4]   ; move the address into dx
+    out dx, al        ; send the byte into the I/O port
+    ret               ; return to the caller
+
+; inb - returns a byte from the given I/O port
+;
+; stack: [esp+4] [ word]the I/O port
+;        [esp  ] [dword]the return address
+inb:
+    mov dx, [esp+4]   ; move the address into dx
+    in al, dx         ; read a byte from the I/O port into al
+    ret               ; return to the caller
