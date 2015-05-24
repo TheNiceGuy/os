e3e7ad905aef0d7c7024dcac9b58c787b879a364
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2015-05-24 18:53:30 -0400

    Change code for testing printing

--- src/kmain.c
+++ src/kmain.c
@@ -1,7 +1,9 @@
-#include "io.h"
 #include "framebuffer.h"
 
-#define TEST_SIZE 100
+#define FILLED "**"
+#define SPACED "  "
+#define false 0
+#define true  1
 
 void sleep() {
     unsigned i, j;
@@ -10,25 +12,76 @@ void sleep() {
         for(j = 0; j < 1000; j++) {}
 }
 
-void kmain() {
-    char str[TEST_SIZE];
-    int i, j, k;
+void draw_limit(int size, int reversed) {
+    int j;
 
-    fb_clear();
-
-    for(j = 0; j < 100; j++) {
-        k = 'A';
-        for(i = 0; i < TEST_SIZE; i++) {
-            if(k > 'z')
-                k = 'A';
-
-            str[i] = k;
-            k++;
+    if(reversed) {
+        for(j = 0; j < size; j++) {
+            if(j >= size/2 || j == 0)
+                fb_write(FILLED);
+            else
+                fb_write(SPACED);
         }
-        str[TEST_SIZE-1]='\0';
-
-        fb_write(str);
-
-        sleep();
+    } else {
+        for(j = 0; j < size; j++) {
+            if(j <= size/2 || j == size-1)
+                fb_write(FILLED);
+            else
+                fb_write(SPACED);
+        }
     }
+    fb_write("\n");
+}
+
+void draw_middle(int size, int reversed) {
+    int j;
+
+    for(j = 0; j < size; j++) {
+        if(reversed) {
+            if(j == 0 || j == (size-1)/2)
+                fb_write(FILLED);
+            else
+                fb_write(SPACED);
+        } else {
+            if(j == size-1 || j == (size-1)/2)
+                fb_write(FILLED);
+            else
+                fb_write(SPACED);
+        }
+    }
+    fb_write("\n");
+}
+
+void draw_middle_line(int size) {
+    int j;
+
+    for(j = 0; j < size; j++) {
+        fb_write(FILLED);
+    }
+    fb_write("\n");
+}
+
+void draw(int size) {
+    int i;
+
+    if(size%2 == 0)
+        size++;
+
+    for(i = 0; i < size; i++) {
+        sleep();
+        if(i == 0)
+            draw_limit(size, false);
+        else if(i == size-1)
+            draw_limit(size, true);
+        else if(i < (size-1)/2)
+            draw_middle(size, false);
+        else if(i > (size-1)/2)
+            draw_middle(size, true);
+        else
+            draw_middle_line(size);
+    }
+}
+
+void kmain() {
+    draw(20);
 }
